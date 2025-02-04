28f8b288dea240dcbc28bf1e877b2eca32789e68
Joshua Lee <joshuadmlee@gmail.com>
2025-02-04 15:59:56 -0800

    lab 5 step 0 fix attempt 2

--- global.js
+++ global.js
@@ -131,8 +131,10 @@ export function renderProjects(project, containerElement, headingLevel = 'h2') {
       article.innerHTML = `
         <${headingLevel}>${proj.title}</${headingLevel}>
         <img src="${proj.image}" alt="${proj.title}">
-        <p>${proj.description}</p>
-        <p class="project-year">${proj.year}</p>
+        <div class="project-details">
+            <p>${proj.description}</p>
+            <p class="project-year">${proj.year}</p>
+        </div>
       `;
       containerElement.appendChild(article);
     });
