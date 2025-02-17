0a3702cce3ee0ed544eaf7c722d6fb69e1871ae8
Joshua Lee <joshuadmlee@gmail.com>
2025-02-17 13:00:09 -0800

    Lab 6 Step 0.1
    adding new page, Meta

--- global.js
+++ global.js
@@ -20,6 +20,7 @@ let pages = [
     { url: 'projects/index.html', title: 'Projects' },
     { url: 'contact/index.html', title: 'Contact' },
     { url: 'resume/index.html', title: 'Resume' },
+    { url: 'meta/index.html', title: 'Meta'},
     { url: 'https://github.com/joshleh', title: 'GitHub Profile' },
     { url: 'https://www.linkedin.com/in/joshleh/', title: 'LinkedIn Profile' },
 ];
